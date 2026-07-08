232b07f4b8110408b221f77eb5ecdc9e2ecdc73f
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-07-08 16:25:30 +0530

    initial commit

---
+++ style.js
@@ -0,0 +1,57 @@
+//Qs1.Write a JS program to get the first n element of an array.
+
+/*let arr = [3, 5, 6, 43, 5, 3, 52, 44, 55, 77, 88, 12, 343, , 57];
+let n = prompt("Enter your number");
+console.log(arr.splice(0, n));
+*/
+
+//Qs2. Write a JS program to get the last n element of an array.
+
+/*
+let arr = [3,4,6,2,1,6,6,8,0,3];
+let pro = prompt("Enter your number");
+let n = arr.length-pro;
+console.log(arr.splice(n));
+*/
+
+//Qs3. Write a JS program to check whether a string is blank or not.
+
+/*
+let str = prompt("Enter your String");
+if(str.length == 0){
+    console.log("String is blank");
+}else{
+    console.log("String in not blank");
+}
+*/
+
+//Qs4. Write JS program to test whether the character at the given character is lowercase.
+
+/*
+let str = "Hello DoSTO";
+let n = prompt('Enter you number');
+if(str[n] <'Z'){
+    console.log("Character is in UpperCase");
+}else{
+    console.log("Character is in LowerCase");
+}
+*/
+
+//Qs5. Write a JS program to check if an element exist in an array or not.
+
+/*
+let arr = [4,3,5,1,2,66,44,3,64,53,34,23];
+let value = Number(prompt("Enter your value"));
+if(arr.indexOf(value) != -1){
+    console.log("element exists in array");
+}else{
+    console.log("element is not exists in array");
+}
+*/
+
+
+//Game Of Guess the Movie;
+
+let movie = "Tarzan";
+let guess = prompt("Enter movie Name");
+
